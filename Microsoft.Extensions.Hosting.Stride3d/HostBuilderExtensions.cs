a915a0ec0fc918c6b12ae7edbfa0d66cbb481977
Adam Johnson <aghjohnson@gmail.com>
2021-07-01 20:27:24 -0500

    init

---
+++ Microsoft.Extensions.Hosting.Stride3d/HostBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting.Stride;
+using Stride.Engine;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.Hosting
+{
+    public static class HostBuilderExtensions
+    {
+        public static IHostBuilder UseStrideLifetime(this IHostBuilder hostBuilder, Game game)
+        {
+            return hostBuilder.ConfigureServices((context, collection) => collection.AddSingleton<IHostLifetime>(new StrideLifetime(game)));
+        }
+
+        public static IHostBuilder UseStrideLifetime<TGame>(this IHostBuilder hostBuilder)
+            where TGame : Game
+        {
+            return hostBuilder.ConfigureServices((context, collection) => collection.AddSingleton<IHostLifetime, StrideLifetime<TGame>>());
+        }
+
+        public static Task RunStrideAsync(this IHostBuilder hostBuilder, Game game, CancellationToken cancellationToken = default)
+        {
+            return hostBuilder.UseStrideLifetime(game).Build().RunAsync(cancellationToken);
+        }
+
+        public static Task RunStrideAsync<TGame>(this IHostBuilder hostBuilder, CancellationToken cancellationToken = default)
+            where TGame : Game
+        {
+            return hostBuilder.UseStrideLifetime<TGame>().Build().RunAsync(cancellationToken);
+        }
+    }
+}
---
+++ Microsoft.Extensions.Hosting.Stride3d/StrideLifetime.cs
@@ -0,0 +1,71 @@
+﻿using Stride.Engine;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.Hosting.Stride
+{
+    public class StrideLifetime : IHostLifetime
+    {
+        private readonly Game _game;
+        private Thread _gameThread;
+
+        public StrideLifetime(Game game)
+        {
+            _game = game;
+            _game.Exiting += _game_Exiting;
+        }
+        private void _game_Exiting(object sender, EventArgs e)
+        {
+            StopAsync(default);
+            _gameThread.Interrupt();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _game.Dispose();
+            return Task.CompletedTask;
+        }
+
+        public async Task WaitForStartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                await Task.Delay(0);
+                _gameThread = Thread.CurrentThread;
+                _game.Run();
+            }
+            catch (ThreadInterruptedException) { }
+        }
+    }
+
+
+    public class StrideLifetime<TGame> : IHostLifetime
+        where TGame : Game
+    {
+        private readonly TGame _game;
+
+        public StrideLifetime(TGame game)
+        {
+            _game = game;
+            _game.Exiting += _game_Exiting;
+        }
+
+        private void _game_Exiting(object sender, EventArgs e)
+        {
+            StopAsync(default);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _game.Dispose();
+            return Task.CompletedTask;
+        }
+
+        public Task WaitForStartAsync(CancellationToken cancellationToken)
+        {
+            _game.Run();
+            return Task.CompletedTask;
+        }
+    }
+}
