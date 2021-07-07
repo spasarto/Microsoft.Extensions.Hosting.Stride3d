f8b61844091e19ecd32ce4e9cb00780080897b34
Adam Johnson <aghjohnson@gmail.com>
2021-07-07 10:41:13 -0500

    Adding support for specifying the GameContext

--- Microsoft.Extensions.Hosting.Stride3d/HostBuilderExtensions.cs
+++ Microsoft.Extensions.Hosting.Stride3d/HostBuilderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting.Stride;
 using Stride.Engine;
+using Stride.Games;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,18 +9,20 @@ namespace Microsoft.Extensions.Hosting
 {
     public static class HostBuilderExtensions
     {
-        public static IHostBuilder UseStrideLifetime(this IHostBuilder hostBuilder)
+        public static IHostBuilder UseStrideLifetime(this IHostBuilder hostBuilder, GameContext gameContext = null)
         {
             return hostBuilder.ConfigureServices((context, collection) =>
             {
                 collection.AddSingleton<Game, Game>();
                 collection.AddSingleton<IHostLifetime, StrideLifetime<Game>>();
+                collection.AddSingleton(new GameContextProvider(gameContext));
             });
         }
 
-        public static Task RunStrideAsync(this IHostBuilder hostBuilder, CancellationToken cancellationToken = default)
+        public static Task RunStrideAsync(this IHostBuilder hostBuilder, GameContext gameContext = null, CancellationToken cancellationToken = default)
         {
-            return hostBuilder.UseStrideLifetime().Build().RunAsync(cancellationToken);
+            return hostBuilder.UseStrideLifetime(gameContext).Build().RunAsync(cancellationToken);
         }
+
     }
 }
--- Microsoft.Extensions.Hosting.Stride3d/StrideLifetime.cs
+++ Microsoft.Extensions.Hosting.Stride3d/StrideLifetime.cs
@@ -1,4 +1,5 @@
 ﻿using Stride.Engine;
+using Stride.Games;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,10 +10,12 @@ namespace Microsoft.Extensions.Hosting.Stride
     {
         private readonly IHostApplicationLifetime _applicationLifetime;
         private readonly TGame _game;
+        private readonly GameContextProvider _gameContextProvider;
 
-        public StrideLifetime(TGame game, IHostApplicationLifetime applicationLifetime)
+        public StrideLifetime(TGame game, GameContextProvider gameContextProvider, IHostApplicationLifetime applicationLifetime)
         {
             _game = game;
+            this._gameContextProvider = gameContextProvider;
             _applicationLifetime = applicationLifetime;
         }
 
@@ -33,10 +36,20 @@ namespace Microsoft.Extensions.Hosting.Stride
                         _applicationLifetime.StopApplication();
                     };
                 };
-                _game.Run();
+                _game.Run(_gameContextProvider.GameContext);
             });
 
             return Task.CompletedTask;
+        }
+    }
+
+    public class GameContextProvider
+    {
+        public GameContext GameContext { get; }
+
+        public GameContextProvider(GameContext gameContext)
+        {
+            GameContext = gameContext;
         }
     }
 }
